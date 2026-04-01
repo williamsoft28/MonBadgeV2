42f6ca42e8cc812e87176848998876e217cc3175
williamsoft28 <wilizbga28@gmail.com>
2026-04-01 03:58:11 +0200

    constaint

---
+++ mobile/lib/utils/constants.dart
@@ -0,0 +1,14 @@
+class Constants {
+  // URL de base de l'API
+  static const String baseUrl = 'http://192.168.137.20:3000/api';
+  
+  // Clés SharedPreferences
+  static const String tokenKey = 'token';
+  static const String userKey = 'user';
+  
+  // Rayon géolocalisation en mètres
+  static const double rayonMax = 15.0;
+  
+  // Durée token en jours
+  static const int tokenDuree = 7;
+}
